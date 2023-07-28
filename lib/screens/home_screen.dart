0f5d19fd56bc28ef0760232af0aab354e556ca37
shalabycr7 <abdoshalaby.dev@gmail.com>
2023-07-28 16:19:21 +0300

    add resposive

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:sports_zone/styles/gradient_decoration.dart';
 import 'package:sports_zone/widgets/card_category.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -25,83 +25,90 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-        body: SafeArea(
-      child: Stack(
-        children: [
-          Container(
-            decoration: blueGradient,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      "Select your favourite sport",
-                      style: GoogleFonts.quicksand(
-                          fontSize: 20.0,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600),
-                    ),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    const Icon(
-                      Icons.arrow_circle_right_outlined,
-                      color: Colors.white,
-                      size: 22,
-                    )
-                  ],
-                ),
-              ],
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              decoration: blueGradient,
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.85,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 245, 240, 252),
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(25),
-                ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: screenHeight * 0.02,
+                horizontal: screenWidth * 0.04,
               ),
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 15,
-                mainAxisSpacing: 15,
-                children: const [
-                  CardCategory(
-                    images: "assets/images/football_badge.png",
-                    sportName: "Football",
-                    brief: "cover all football leagaues",
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Select your favourite sport",
+                    style: GoogleFonts.quicksand(
+                      fontSize: screenHeight * 0.024,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                  CardCategory(
-                    images: "assets/images/tennis_badge.png",
-                    sportName: "Tennis",
-                    brief: "cover all tennis championships",
+                  const SizedBox(
+                    width: 10,
                   ),
-                  CardCategory(
-                    images: "assets/images/basketball_badge.png",
-                    sportName: "Basketball",
-                    brief: "cover all basketball teams",
-                  ),
-                  CardCategory(
-                    images: "assets/images/cricket_badge.png",
-                    sportName: "Cricket",
-                    brief: "cover all cricket tournaments",
-                  ),
+                  Icon(
+                    Icons.arrow_circle_right_outlined,
+                    color: Colors.white,
+                    size: screenHeight * 0.03,
+                  )
                 ],
               ),
             ),
-          )
-        ],
+            Positioned(
+              bottom: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: screenHeight * 0.02,
+                  horizontal: screenWidth * 0.04,
+                ),
+                width: screenWidth,
+                height: screenHeight * 0.85,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 245, 240, 252),
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(25),
+                  ),
+                ),
+                child: GridView.count(
+                  crossAxisCount: screenWidth > 600 ? 4 : 2,
+                  crossAxisSpacing: screenWidth * 0.04,
+                  mainAxisSpacing: screenWidth * 0.04,
+                  children: const [
+                    CardCategory(
+                      images: "assets/images/football_badge.png",
+                      sportName: "Football",
+                      brief: "cover all football leagaues",
+                    ),
+                    CardCategory(
+                      images: "assets/images/tennis_badge.png",
+                      sportName: "Tennis",
+                      brief: "cover all tennis championships",
+                    ),
+                    CardCategory(
+                      images: "assets/images/basketball_badge.png",
+                      sportName: "Basketball",
+                      brief: "cover all basketball teams",
+                    ),
+                    CardCategory(
+                      images: "assets/images/cricket_badge.png",
+                      sportName: "Cricket",
+                      brief: "cover all cricket tournaments",
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/widgets/card_category.dart
+++ lib/widgets/card_category.dart
@@ -48,47 +48,45 @@ class CardCategory extends StatelessWidget {
         }
       },
       child: Container(
-        width: double.infinity,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10.0),
           color: Colors.white,
         ),
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        images,
-                        width: 80,
-                        height: 80,
-                      ),
-                      Text(
-                        sportName,
-                        style: GoogleFonts.quicksand(
-                          fontSize: 21,
-                          color: Colors.blue,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        textAlign: TextAlign.center,
-                        brief,
-                        style: GoogleFonts.quicksand(
-                          fontSize: 14,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
+            Expanded(
+              child: Image.asset(
+                images,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Flexible(
+              child: Text(
+                sportName,
+                style: GoogleFonts.quicksand(
+                  fontSize: 21,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  brief,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.quicksand(
+                    fontSize: 14,
+                    color: Colors.black,
                   ),
                 ),
-              ],
+              ),
             ),
+            const SizedBox(height: 10),
           ],
         ),
       ),
